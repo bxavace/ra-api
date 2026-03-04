93b24b4669c245d3f3541a166aefb6193d19f1fb
Brylle Nuñez <b_xavace@outlook.com>
2026-03-04 10:06:46 +0800

    add: implement metadata endpoint to retrieve assessment-related data

--- app/routes.py
+++ app/routes.py
@@ -1,5 +1,11 @@
 from flask import Blueprint, request, jsonify
 from app.services.assessment_service import create_assessment
+from app.models import (
+    GlobalProcess, ProcessLevel, AssetCrownJewels, PolicyRegulation, Vulnerability,
+    ThreatSource, ImpactCategory, RiskResponse
+)
+from app.constants.impact_matrix import IMPACT_MATRIX
+from app.constants.risk_table import RISK_TABLE
 
 bp = Blueprint("api", __name__, url_prefix="/api")
 
@@ -11,3 +17,34 @@ def submit_assessment():
         return jsonify({"message": "Assessment submitted successfully.", "assessment_id": result["assessment_id"]}), 201
     else:
         return jsonify({"error": result["error"]}), 400
+
+@bp.route("/metadata", methods=["GET"])
+def get_metadata():
+    return {
+        "global_processes": [
+            {"id": x.id, "name": x.name, "description": x.description} for x in GlobalProcess.query.all()
+        ],
+        "process_levels": [
+            {"id": x.id, "name": x.name, "description": x.description} for x in ProcessLevel.query.all()
+        ],
+        "asset_crown_jewels": [
+            {"id": x.id, "name": x.name, "description": x.description} for x in AssetCrownJewels.query.all()
+        ],
+        "policies": [
+            {"id": x.id, "name": x.name, "description": x.description} for x in PolicyRegulation.query.all()
+        ],
+        "vulnerabilities": [
+            {"id": x.id, "global_process_id": x.global_process_id, "description": x.description} for x in Vulnerability.query.all()
+        ],
+        "threat_sources": [
+            {"id": x.id, "name": x.name, "description": x.description, "threat_category": x.threat_category, "initiation_type": x.initiation_type, "actor": x.actor, "action": x.action} for x in ThreatSource.query.all()
+        ],
+        "impact_categories": [
+            {"id": x.id, "name": x.name, "description": x.description} for x in ImpactCategory.query.all()
+        ],
+        "risk_responses": [
+            {"id": x.id, "name": x.name, "guidance": x.guidance} for x in RiskResponse.query.all()
+        ],
+        "impact_matrix": IMPACT_MATRIX,
+        "risk_table": RISK_TABLE,
+    }
